d64533390e1c91e8e4b7e4e2283570e99b9565ed
lin.ye@philips.com <Passw0rd^159753123>
2018-06-05 18:07:41 +0800

    add ai_id to dialog table

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,4 @@
-#coding=utf8
+# coding=utf8
 import werobot
 import json
 import requests
@@ -13,30 +13,37 @@ sql_dao = mysql_dao()
 Answerclass = Answer()
 
 robot = werobot.WeRoBot(enable_session=False,
-                        token=os.getenv('WECHAT_TOKEN','trueai'),
-                        APP_ID=os.getenv('WECHAT_APP_ID','wx63efb6f9efadb72b'),
-                        APP_SECRET=os.getenv('WECHAT_APP_SECRET','45a4a1cdd8bd13e44e9ce26e763d931e'))
+                        token=os.getenv('WECHAT_TOKEN', 'trueai'),
+                        APP_ID=os.getenv(
+                            'WECHAT_APP_ID', 'wx63efb6f9efadb72b'),
+                        APP_SECRET=os.getenv('WECHAT_APP_SECRET', '45a4a1cdd8bd13e44e9ce26e763d931e'))
 client = robot.client
 
-def log_dialog(userID, question, coreferenceQuestion, answer):
-    sql_dao.insert_dialog(userID,question,coreferenceQuestion,answer)
+
+def log_dialog(userID, question, coreferenceQuestion, answer, ai_id):
+    return sql_dao.insert_dialog(userID, question, coreferenceQuestion, answer, ai_id)
+
 
 def log_ai_procedure(question, intention, ir_answer, comprehen_answer):
-    sql_dao.insert_ai_procedure(question, intention, ir_answer, comprehen_answer)
+    return sql_dao.insert_ai_procedure(question, intention, ir_answer, comprehen_answer)
+
 
 def handle_question_from_user(userID, question):
     intention, scorevesus = Answerclass.getIntention(question)
     print(intention)
     if intention == 'other':
         other_answer = Answerclass.getOtherAnswer(question)
-        log_ai_procedure(question, intention, None, other_answer)
-        log_dialog(userID, question, question, other_answer)
+        ai_id = log_ai_procedure(question, intention, None, other_answer)
+        log_dialog(userID, question, question, other_answer, ai_id)
         return other_answer
     else:
-        ir_answer, coreference_question, result_json = Answerclass.getIRAnswer(question)
-        log_ai_procedure(question, intention, json.dumps(result_json, ensure_ascii=False), ir_answer)
-        log_dialog(userID, question, coreference_question, ir_answer)
+        ir_answer, coreference_question, result_json = Answerclass.getIRAnswer(
+            question)
+        ai_id = log_ai_procedure(question, intention, json.dumps(
+            result_json, ensure_ascii=False), ir_answer)
+        log_dialog(userID, question, coreference_question, ir_answer, ai_id)
         return ir_answer
+
 
 @robot.handler
 def answerQuestion(message):
@@ -49,6 +56,6 @@ def answerQuestion(message):
 
 
 if __name__ == "__main__":
-    question = "Hello";
+    question = "乳腺癌饮食该注意什么？"
     answer = handle_question_from_user("source", question)
     print(answer)
--- mysql_dao.py
+++ mysql_dao.py
@@ -1,47 +1,56 @@
 # -*- coding: utf-8 -*-
 import mysql.connector
 
-def singleton(cls, *args, **kw):  
-    instances = {}  
-    def _singleton():  
-        if cls not in instances:  
-            instances[cls] = cls(*args, **kw)  
-        return instances[cls]  
+
+def singleton(cls, *args, **kw):
+    instances = {}
+
+    def _singleton():
+        if cls not in instances:
+            instances[cls] = cls(*args, **kw)
+        return instances[cls]
     return _singleton
+
 
 @singleton
 class mysql_dao:
 
-    def insert_dialog(self, userid, question, coreference_question, answer):
+    def insert_dialog(self, userid, question, coreference_question, answer, ai_id):
         if userid is None or question is None or coreference_question is None or answer is None:
             raise ValueError('some param is none')
-        cnx = mysql.connector.connect(user='cdidev', password='Philips@123',host='rm-uf6e87o934505d1162o.mysql.rds.aliyuncs.com',database='trueview_chatbot')
+        cnx = mysql.connector.connect(user='cdidev', password='Philips@123',
+                                      host='rm-uf6e87o934505d1162o.mysql.rds.aliyuncs.com', database='trueview_chatbot')
         cursor = cnx.cursor()
 
-        cursor.callproc("insert_dialog_manage",(None,userid, None, question,coreference_question))
+        cursor.callproc("insert_dialog_manage", (None, userid,
+                                                 None, question, coreference_question))
         cursor.execute("SELECT LAST_INSERT_ID();")
-        dialog_manage_id=cursor.fetchone()
+        dialog_manage_id = cursor.fetchone()
 
-        cursor.callproc("insert_dialog",(userid,None, question, answer,None,dialog_manage_id[0],None))
+        cursor.callproc("insert_dialog", (userid, None, question,
+                                          answer, ai_id, dialog_manage_id[0], None))
         cursor.execute("SELECT LAST_INSERT_ID();")
-        dialog_id=cursor.fetchone()
+        dialog_id = cursor.fetchone()
 
-        cursor.execute("UPDATE `dialog_manage` SET `Dialog_id` = %s WHERE `Id` = %s;",(dialog_id[0],dialog_manage_id[0]))
+        cursor.execute("UPDATE `dialog_manage` SET `Dialog_id` = %s WHERE `Id` = %s;",
+                       (dialog_id[0], dialog_manage_id[0]))
 
         cnx.commit()
-        cursor.close();
-        cnx.close();
-        return dialog_id[0];
+        cursor.close()
+        cnx.close()
+        return dialog_id[0]
 
     def insert_ai_procedure(self, question, intention, ir_answer, comprehen_answer):
         if question is None or intention is None or comprehen_answer is None:
             raise ValueError('some param is none')
-        cnx = mysql.connector.connect(user='cdidev', password='Philips@123',host='rm-uf6e87o934505d1162o.mysql.rds.aliyuncs.com',database='trueview_chatbot')
+        cnx = mysql.connector.connect(user='cdidev', password='Philips@123',
+                                      host='rm-uf6e87o934505d1162o.mysql.rds.aliyuncs.com', database='trueview_chatbot')
         cursor = cnx.cursor()
-        cursor.callproc("insert_ai_procedure",(None,question, intention, ir_answer, None, None, comprehen_answer, None, 0, None))
+        cursor.callproc("insert_ai_procedure", (None, question, intention,
+                                                ir_answer, None, None, comprehen_answer, None, 0, None))
         cursor.execute("SELECT LAST_INSERT_ID();")
-        ai_procedure_id=cursor.fetchone()
+        ai_procedure_id = cursor.fetchone()
         cnx.commit()
-        cursor.close();
-        cnx.close();
+        cursor.close()
+        cnx.close()
         return ai_procedure_id[0]
