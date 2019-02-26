a43df43dbe6b4b6e4cf74cd08349f9d9154148bc
tmacraft <tmacraft@hotmail.com>
2019-02-26 11:40:32 +0800

    Revert "pop up question when classification result won't match db"
    This reverts commit c062b0af95d1e1c56208d2085ec771bc8e150fbd.

--- chatbot.py
+++ chatbot.py
@@ -140,13 +140,11 @@ class RobotService():
 
         disease_classification = self.classifier(question, '疾病')
 
-        if disease_classification == 'other' or disease_classification is None:
+        if disease_classification == 'other' or disease_classification == None:
             if disease_db:
                 return disease_db
             else:
                 return None
-        elif disease_classification != disease_db:
-            return None
         else:
             return disease_classification
 
@@ -157,7 +155,7 @@ class RobotService():
 
         topic_classification = self.classifier(question, disease_type)
 
-        if topic_classification == 'other' or topic_classification == None or topic_classification != topic_db:
+        if topic_classification == 'other' or topic_classification == None:
              return None
         else:
             return topic_classification
